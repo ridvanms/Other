e4286d84c6bd82ea11d0946ab1a992cc806fc5c7
Ridvan <ridvans@abv.bg>
2023-02-26 14:57:58 +0200

    updating

--- writingONC++/ConsoleApplication1/ConsoleApplication1.cpp
+++ writingONC++/ConsoleApplication1/ConsoleApplication1.cpp
@@ -103,51 +103,120 @@ using namespace std;
       izpolzva 2 ukazatelq
       podobno ot functicqta algorithm
     */
-void print(int* start, int* end) {
-    for (int* p{ start }; p < end; p++) {
-        cout << *p << " ";
-    }
-    cout << "\n" << endl;
-}
-void heap_array_pointers() {
-    
-
-    //Zadachata imitira teglene na chisla za toto 6/49,5/35
-
-    system("chcp 1251>nul");
-    //Razmerut na masiv v heap moze da e promenliva 
-    //Vednuj zadaden ne se izmenq 
-    int count;
-    cout << "Obsht broi chisla?";
-    cin >> count; //example 49
-    //Suzdavame masiv v heap s razmer count
-    int* nums = new int[count] {};
-    cout << "Izhoden masiv\n";
-    print(nums, nums + count);
-
-    cout << "sizeof nums= " << sizeof(nums) << endl;
-
-    /*
-        Za masivite v heapa polzvame 
-       cikup po index i cikul po ukazatel
-    */
-    
-    //Zapulvame masiva nums s chislata ot 1 do count
-    for (int i{ 0 }; i < count; i++) {
-        nums[i] = i + 1;
-    }
-    cout << "Zapulnen izhoden masiv\n";
-    print(nums, nums + count);
-
-
-};
+//void print(int* start, int* end) {
+//    for (int* p{ start }; p < end; p++) {
+//        cout << *p << " ";
+//    }
+//    cout << "\n" << endl;
+//}
+//void heap_array_pointers() {
+//    
+//
+//    //Zadachata imitira teglene na chisla za toto 6/49,5/35
+//
+//    system("chcp 1251>nul");
+//    //Razmerut na masiv v heap moze da e promenliva 
+//    //Vednuj zadaden ne se izmenq 
+//    int count;
+//    cout << "Obsht broi chisla?";
+//    cin >> count; //example 49
+//    //Suzdavame masiv v heap s razmer count
+//    int* nums = new int[count] {};
+//    cout << "Izhoden masiv\n";
+//    print(nums, nums + count);
+//
+//    cout << "sizeof nums= " << sizeof(nums) << endl;
+//
+//    /*
+//        Za masivite v heapa polzvame 
+//       cikup po index i cikul po ukazatel
+//    */
+//    
+//    //Zapulvame masiva nums s chislata ot 1 do count
+//    for (int i{ 0 }; i < count; i++) {
+//        nums[i] = i + 1;
+//    }
+//    cout << "Zapulnen izhoden masiv\n";
+//    print(nums, nums + count);
+//
+//    //razburkvame chislata 
+//    //inicializaciq na generatora na sluch chisla
+//    srand(time(0));
+//    random_shuffle(nums, nums + count);
+//    cout << "razburkan masiv\n";
+//    print(nums, nums + count);
+//
+//    int sel_count; //broi izbrani chisla za igra 5 ili 6 za Bulgariq
+//    cout << "S kolko chisla shte igraete?: ";
+//    cin >> sel_count;
+//
+//    //Suzdavame masiv v heapa za izbranite chisla
+//    int* sel_numbers = new int[sel_count];
+//
+//    //1 variant - kopirane purvite sel_count chisla ot nums
+//    //V noviq masiv
+//    for (size_t i = 0; i < sel_count; i++) {
+//        sel_numbers[i] = nums[i];
+//    }
+//    cout << "Var 1 : izbrani chisla\n ";
+//    print(sel_numbers, sel_numbers + sel_count);
+//    cin.get();
+//    cin.get();//pausa do natiskane na simvol
+//    system("cls");//izchistvame ekrana
+//
+//    /*
+//        2 Variant prid da 'izteglim chislo'
+//      vseki put razburkvame ostanalata chast ot masiva nums,
+//      no ne uchastva v sledvashite razburkvaniq(t.e ne e tochnokato najivo)
+//
+//    */
+//    //Zapochvame otnachalo - zapulvame masiva nums s chislata 
+//    // ot 1 do count
+//    for (int i{ 0 }; i < count; i++) {
+//        nums[i] = i + 1;
+//    }
+//    cout << "Zapulnen izhoden masiv\n";
+//    print(nums, nums + count);
+//    int k{ 0 }; //k - index na porednoto chislo va masiva  sel_numbers , zapochvame ot nula
+//    
+//    //cikul po broq nachislata s koito shte igraem
+//    for (size_t i = 0; i < sel_count; i++) {
+//        /*
+//            Pri vsqkosledvasho razburkvane izpolzvame
+//            OSTANALATA  chast ot masiva bez veche 'izteglenite' chisla
+//            t.e kato nachalen adres na masiva zadavame nums + i
+//        */
+//        random_shuffle(nums + i, nums + count);
+//        cout << "Sled " << i + 1 << " razburkvane\n";
+//        print(nums, nums + count);
+//
+//        //sled purvoto rzburkvane shte vzemem ("izteglqme") chisloto
+//        //koeto e na 1-va poziciq (s index 0)
+//        sel_numbers[k] = nums[k];
+//        //Sled vtoroto razburkvane "izteglqme"chisloto
+//        //koeto e na 2-ra poziciq (s index 1)i t.n
+//        k++; //index na elementa, koito shte teglim sled sledvashtoto razburkvane 
+//    }
+//    cout << "Var 2: izbrani chisla\n";
+//    print(sel_numbers, sel_numbers + sel_count);
+//    //podrejdamechislata po narastvane s bibliotechna funkciq sort
+//    sort(sel_numbers, sel_count + sel_numbers);
+//    cout << "Sortirani po narastvane\n";
+//    print(sel_numbers, sel_numbers + sel_count);
+//
+//    delete[] nums; // osvobojdavame pametta v heapa zaemana ot nums
+//    delete[] sel_numbers;
+//    nums = nullptr;//nulirame ukazatelite
+//    sel_numbers = nullptr;
+//    
+//};
 int main() {
     heap_array_pointers();
 
     //const_with_pointers();
 
     //stack_array_pointers();
-    
+
     return 0;
 }
 
