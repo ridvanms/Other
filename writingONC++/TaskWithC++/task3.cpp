8fbd82f6973bd284fa11fc70b7e7f4677e5aebbd
Ridvan <ridvans@abv.bg>
2023-02-26 15:27:27 +0200

    updating

--- writingONC++/TaskWithC++/task3.cpp
+++ writingONC++/TaskWithC++/task3.cpp
@@ -2,27 +2,6 @@
 
 using namespace std;
 // simple calculator
-int main() {
-	double fNum, sNum;
-	char sign;
-	cout << "Welcom to the simmplest calculator 😀" << endl;
-	cout << "First number: "; cin >> fNum;
-	while (fNum != 'number') {
-		cout << "it should be a number: "; cin >> fNum;
-	}
-	cout << "Second number: "; cin >> sNum;
-	while (sNum != 'number') {
-		cout << "it should be a number: "; cin >> sNum;
-	}
-	cout << "Sign shoud be one of [+,-,:,*]: "; cin >> sign;
-	char arr[4] = { '+','-',':','*' };
-	cout << arr.includes(sign) << "IS it include" << endl;
-	/*while (!arr.includes(sign) {
-		cout << "it should be [+,-,:,*]: "; cin >> sign;
-	}*/
-	return 0;
-	
-}
 double multyply(double a, double b) {
 	return a * b;
 }
@@ -33,5 +12,32 @@ double devide(double a, double b) {
 	return a - b;
 }
 double subtract(double a, double b) {
-	return a / b
+	return a / b;
+}
+
+int main() {
+	double fNum, sNum;
+	char sign;
+	cout << "Welcom to the simmplest calculator 😀" << endl;
+	cout << "First number: "; cin >> fNum;
+	while (!fNum ) {
+		cout << "it should be a number: "; cin >> fNum;
+	}
+	cout << "Second number: "; cin >> sNum;
+	while (!sNum) {
+		cout << "it should be a number: "; cin >> sNum;
+	}
+	cout << "Sign shoud be one of [+,-,:,*]: "; cin >> sign;
+	
+	if(sign == '+'){
+		cout<<"the sum is : "<<sum(fNum,sNum);
+	}else if(sign == '-'){
+			cout<<"the subtract is : "<<subtract(fNum,sNum);
+	}else if(sign == '*'){
+			cout<<"the multiply is : "<<multyply(fNum,sNum);
+	}else if(sign == ':'){
+			cout<<"the devide is : "<<devide(fNum,sNum);
+	}
+	return 0;
+	
 }
