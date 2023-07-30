7839f71c080fa4f9c1426f60ca9343bf36b922ff
Ridvan <ridvans@abv.bg>
2023-07-30 13:47:26 +0300

    start learning java

---
+++ JavaLearning/java_1/quickStart.java
@@ -0,0 +1,9 @@
+class QuickStart {
+    public static void main (String[] args) {
+        System.out.println("Hello, World.");
+        System.out.print(summing());
+    }
+    public static Number summing(){
+        return 5;
+    }
+}
