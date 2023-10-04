06c7cb7b119245fd7c0a98799bc59b7114310a0a
Ridvan <ridvans@abv.bg>
2023-10-04 18:30:18 +0300

    updating

--- JavaLearning/java_1/quickStart.java
+++
@@ -1,9 +0,0 @@
-class QuickStart {
-    public static void main (String[] args) {
-        System.out.println("Hello, World.");
-        System.out.print(summing());
-    }
-    public static Number summing(){
-        return 5;
-    }
-}
