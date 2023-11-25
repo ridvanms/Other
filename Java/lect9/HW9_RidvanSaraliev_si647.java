052193ed9646f6f8f8c14ffd2b4cab308664c2ef
Ridvan <ridvans@abv.bg>
2023-11-25 14:22:00 +0200

    updating

--- Java/lect9/HW9_RidvanSaraliev_si647.java
+++ Java/lect9/HW9_RidvanSaraliev_si647.java
@@ -1,94 +1,111 @@
-public class HW6_RidvanSaraliev_si647 {
+public class HW9_RidvanSaraliev_si647 {
+    enum Coffee {
+        ESPRESSO("Еспресо", 2.50),
+        LATTE("Лате", 3.00),
+        CAPUPUCCINO("Капучино", 2.80);
+
+        private String name;
+        private double price;
+
+        Coffee(String name, double price) {
+            this.name = name;
+            this.price = price;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public double getPrice() {
+            return price;
+        }
+
+        public void setPrice(double price) {
+            this.price = price;
+        }
+
+        @Override
+        public String toString() {
+            return name;
+        }
+    }
+
+    enum Sandwich {
+        BLT("БЛТ", 3.50),
+        TUNA("Туна", 4.00),
+        CHICKEN("Чички", 4.50);
+
+        private String name;
+        private double price;
+
+        Sandwich(String name, double price) {
+            this.name = name;
+            this.price = price;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public double getPrice() {
+            return price;
+        }
+
+        public void setPrice(double price) {
+            this.price = price;
+        }
+
+        @Override
+        public String toString() {
+            return name;
+        }
+    }
+
+    static class Order {
+        private Coffee coffee;
+        private Sandwich sandwich;
+
+        public Order(Coffee coffee, Sandwich sandwich) {
+            this.coffee = coffee;
+            this.sandwich = sandwich;
+        }
+
+        public Coffee getCoffee() {
+            return coffee;
+        }
+
+        public void setCoffee(Coffee coffee) {
+            this.coffee = coffee;
+        }
+
+        public Sandwich getSandwich() {
+            return sandwich;
+        }
+
+        public void setSandwich(Sandwich sandwich) {
+            this.sandwich = sandwich;
+        }
+
+        public double getPrice() {
+            return coffee.getPrice() + sandwich.getPrice();
+        }
+
+        @Override
+        public String toString() {
+            return "Order: " + coffee.getName() + " and " + sandwich.getName();
+        }
+    }
+
     public static void main(String[] args) {
-        String result1 = getTotalString("OOP2", "KM");
-        String result2 = getTotalString(result1, "BD", "CAA", "II");
+        Order[] orders = new Order[3];
+        orders[0] = new Order(Coffee.ESPRESSO, Sandwich.BLT);
+        orders[1] = new Order(Coffee.CAPUPUCCINO, Sandwich.CHICKEN);
+        orders[2] = new Order(Coffee.LATTE, Sandwich.TUNA);
 
-        System.out.println("Took exams for the first semester: " + result1);
-        System.out.println("Took exams for the both semesters: " + result2);
-    }
-
-    public static String getTotalString(String... s) {
-      StringBuilder sb = new StringBuilder();
-      for (int i = 0; i < s.length; i++) {
-        sb.append(s[i]);
-        if (i < s.length - 1) {
-          sb.append(", ");
-        } else {
-          sb.append("!");
+        for (Order order : orders) {
+            System.out.println(order.toString());
+            System.out.println("Price: " + order.getPrice());
+            System.out.println();
         }
-      }
-      return sb.toString();
-    }
-}
-
-class Stock {
-    private String title;
-    private double price;
-    private int count;
-
-    public Stock() {
-        // Default constructor
-    }
-
-    public Stock(String title, double price, int count) {
-        this.title = title;
-        this.price = price;
-        this.count = count;
-    }
-
-    public void input() {
-        // Input data from console
-    }
-
-    public void input(String filename) {
-        // Input data from file
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        if (price < 0) {
-            throw new IllegalArgumentException("Price cannot be negative");
-        }
-        this.price = price;
-    }
-
-    public int getCount() {
-        return count;
-    }
-
-    public void setCount(int count) {
-        if (count < 0) {
-            throw new IllegalArgumentException("Count cannot be negative");
-        }
-        this.count = count;
-    }
-
-    public double total() {
-        return price * count;
-    }
-
-    public void discount(int percent) {
-        double discountAmount = price * percent / 100;
-        price -= discountAmount;
-    }
-
-    @Override
-    public String toString() {
-        return "Stock{" +
-                "title='" + title + '\'' +
-                ", price=" + price +
-                ", count=" + count +
-                '}';
     }
 }
